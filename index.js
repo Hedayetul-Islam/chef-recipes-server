c47d0e33f257284fea9f3c421e0038a4819c0c23
Hedayetul-Islam <hedayetul36@gmail.com>
2023-05-05 16:29:50 +0600

    recipes data

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ const port = process.env.PORT || 5000;
 
 const chefData = require('./data.json');
 
+const recipes = require('./recipes.json')
+
 app.use(cors());
 
 app.get('/data', (req, res) => {
     res.send(chefData)
 });
+
+app.get('/recipes/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const chefRecipes = recipes.find(recipe => recipe.serialNumber === id);
+    res.send(chefRecipes)
+})
+
+app.get('/recipes',(req,res) => {
+    res.send(recipes)
+})
 
 app.listen(port, () => {
     console.log('server running')
