21a99bddd17e7dc8efaaed12215efaeb35afe050
Hedayetul-Islam <hedayetul36@gmail.com>
2023-05-05 21:03:41 +0600

    final commit

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.get('/data', (req, res) => {
 app.get('/recipes/:id', (req, res) => {
     const id = req.params.id;
     console.log(id);
-    const chefRecipes = recipes.find(recipe => recipe.serialNumber === id);
-    res.send(chefRecipes)
+    const chefRecipes = recipes.find(recipe => recipe.serialNumber == id);
+    res.send({chefRecipes})
 })
 
 app.get('/recipes',(req,res) => {
